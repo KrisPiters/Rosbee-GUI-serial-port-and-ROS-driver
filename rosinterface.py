46fbb5fd320e5301b229d9b85470df051a2afd4c
Probotics BV <hkiela@opteq.nl>
2016-02-24 23:46:57 +0100

    rosinterface.oy Ros interface wrapper added to hide rbha.py. And rbha.py updated for ros interface
    To create an as clean as possible Rosbee hardware abstraction layer, the rosinterface.py has been added. This should hold everything needed for the ros device driver.

---
+++ rosinterface.py
@@ -0,0 +1,31 @@
+# import module for ROS.
+# Provides a wrapper around the rosbee hardware abstraction module
+# Opteq febr 2016
+
+import rbha.py
+
+# Disable of robot
+def disable_robot():
+    rbha.disable_robot()
+
+# Enable of robot
+def enable_robot():
+    rbha.enable_robot()
+
+# request robot enable status. Returns true if enabled and false if disabled
+def request_enable_status():
+    return rbha.request_enable_status()
+
+# request alarm bit status of robot. Returns true if any alarm set
+def request_alarm():
+    return rbha.request_alarm()
+
+# Get actual move speed and rotational speed of robot in SI units
+# Reports calculated speed x from motor encoders and robot rotation based on either encoders of gyro depending on gyrobased being true
+def get_movesteer(gyrobased):
+    rbha.get_movesteer(gyrobased)
+
+# Move command. Input in SI units. speed in m/s dir in radians per sec
+def do_movesteer(speed, rot_z):
+    rbha.do_movesteer(speed, rot_z)
+
